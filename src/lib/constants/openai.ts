6f452a3d63f018f937f1ba4a336d68d44f738d02
Per Ersson <per@acuminor.com>
2023-11-07 10:34:10 +0100

    Added gpt4 preview support

--- src/lib/constants/openai.ts
+++ src/lib/constants/openai.ts
@@ -3,6 +3,7 @@ export const CHATGPT_MODELS = Object.freeze({
   "gpt-3.5-turbo-16k": { label: "GPT-3.5 turbo 16k", tokens: 16384 },
   "gpt-4": { label: "GPT-4", tokens: 8192 },
   "gpt-4-32k": { label: "GPT-4 32k", tokens: 32768 },
+  "gpt-4-1106-preview": { label: "GPT-4 Turbo preview", tokens: 128000 },
 });
 
 export type ChatGPTModel = keyof typeof CHATGPT_MODELS;
